e2f7816c5d4f951df5d1b58d9e905c6499f94042
Name <email@example.ru>
2021-01-06 13:04:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp267/Program.vb
@@ -0,0 +1,13 @@
+Using System;
+
+Class MainClass {
+  Public Static void Main (String[] args) {
+
+	 String myName;
+	 myName = "Jane";
+
+	 Console.WriteLine(myName);
+	 
+	 Console.ReadKey();
+  }
+}
